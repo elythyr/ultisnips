69914deb8192edd6c71f476a6d08bd245997df8f
Camille Dejoye <cdejoye@houra.fr>
2018-11-04 15:32:22 +0100

    Initializes the branch for the issue I will report.
    No modifications is needed or anything, the basic repro docker image is
    enough to reproduce the issue.
    Just open the issue.php file and follow the instructions

---
+++ issue.php
@@ -0,0 +1,15 @@
+<?php
+
+class Test
+{
+	public function first()
+	{
+	}
+
+	// Place the cursor at the after the snippet "mu" and expand it
+	// then leave Insert Mode with <Esc>
+	// Finaly launch the command: put! =nr2char(10) | call UltiSnips#Anon("test")
+	// A new line testppublic is added above the method
+	// When it should actually only be test
+	mu
+}
